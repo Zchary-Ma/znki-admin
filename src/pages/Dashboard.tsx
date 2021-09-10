e4eef33a94983a44876a607abd141e84ccb1348a
Zchary-Ma <Zcharyma@gmail.com>
2021-09-10 14:28:50 +0800

    chore

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,55 @@
-import React, { FC } from 'react';
-import { Box, Typography } from '@material-ui/core';
+import React, { FC, useState } from 'react';
+import { Box } from '@material-ui/core';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import Counter from '../components/Counter';
+import PersonIcon from '@material-ui/icons/Person';
 
-const Dashboard: FC = (props) => <Box>Dashboard</Box>;
-Dashboard.propTypes = {
-  // bla: PropTypes.string,
-};
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    totalView: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      width: '100%',
+    },
+  }),
+);
 
-Dashboard.defaultProps = {
-  // bla: 'test',
+const Dashboard: FC = (props) => {
+  const classes = useStyles();
+
+  const [data, setData] = useState([
+    {
+      name: 'Total Cards',
+      count: 100,
+      icon: <PersonIcon color="disabled" style={{ fontSize: '56px' }} />,
+    },
+    {
+      name: 'Total Cards',
+      count: 100,
+      icon: <PersonIcon color="disabled" style={{ fontSize: '56px' }} />,
+    },
+    {
+      name: 'Total Cards',
+      count: 100,
+      icon: <PersonIcon color="disabled" style={{ fontSize: '56px' }} />,
+    },
+  ]);
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'background.default',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        minHeight: '100vh',
+        padding: 2,
+      }}
+    >
+      <section className={classes.totalView}></section>
+    </Box>
+  );
 };
 
 export default Dashboard;
